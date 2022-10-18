dbfb7ebf3a3a595840dec32e426a0d42dc0c24f7
STS670-PRAVEENRAJ P <praveenrajp199911@gmail.com>
2022-10-18 16:33:39 +0530

    Add project files.

---
+++ FirstPage/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using FirstPage.Models;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace FirstPage.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        
+        public ActionResult FirstPage()
+        {
+            return View();
+        }
+        public ActionResult Index(Mod user)
+        {
+            if (user.userName == "Praveen" && user.pass == 123)
+            {
+                return RedirectToAction("NextPage");
+            }
+            else
+            {
+
+                return RedirectToAction("NextOne");
+            }
+
+        }
+        public ActionResult NextPage(Mod user)
+        {
+            
+                Mod mod = new Mod()
+                {
+                    userName = user.userName,
+                    pass = user.pass,
+                    c = 79,
+                    Java = 89,
+                    Matrics = 65,
+                    Trignometry = 90,
+                    AdvancedAlgebra = 88,
+                    Algebra = 78,
+                    Differential = 65,
+                    Html = 79,
+                    Integral = 89,
+                    OperationResearch = 65,
+                    Python = 90,
+                    Vertices = 78,   
+
+                };
+                return View(mod);
+
+            }
+
+        public ActionResult NextOne()
+        {
+            ViewBag.name = "InCorrect userName and Password";
+            return View();
+        }
+
+    }
+        
+    }
---
+++ FirstPage/Models/Mod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstPage.Models
+{
+    public class Mod
+    {
+        public string userName { get; set; }
+        public int pass { get; set; }
+        public int SemesterOne { get; set; }
+
+        public int SemesterTwo { get; set; }
+        public int SemesterThree { get; set; }
+        public int SemesterFour { get; set; }
+        public int Algebra { get; set; }
+        public int Trignometry { get; set; }
+
+        public int  OperationResearch { get; set; }
+        public int c { get; set; }
+        public int  Matrics { get; set; }
+        public int Differential { get; set; }
+        public int Integral { get; set; }
+
+        public int Vertices { get; set; }
+        public int Java { get; set; }
+        public int AdvancedAlgebra { get; set; }
+        public int Html { get; set; }
+        public int Python { get; set; }
+
+    }
+}
